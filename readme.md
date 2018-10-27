14f04d4e805c62775b088c5c2955d172e5c864b6
Andrew Ferrer <andb.ferrer@gmail.com>
2018-10-27 16:39:44 -0400

    some async checks and planning

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 its coming together
+stretch:
+    grab active chrome/browser window's page
+    run script on page's receipt of complete application
--- yon.js
+++ yon.js
@@ -1,16 +1,32 @@
 const fs = require('fs');
 const now = Date.now();
 const andNow = Date.now() - now;
-console.log(now);
+console.log(` Present time at beginning: ${now}`);
 
 const data = new Uint8Array(Buffer.from('Hello Node.js'));
 fs.writeFile('message.txt', data, (err) => {
   if (err) throw err;
-  console.log('The file has been saved!');
+  console.log(' The file has been saved!');
 });
 
+console.log(` Present time at Completion: ${andNow}`);
+
+console.log(` This is the second argument: ${process.argv[1]}`);
+
+// receive some input url to scrape
+
+// determine what job board the page is from, will have to be specific for dice, indeed, etc.
 
 
+// scrape page for relevant data: divs, links, etc.
 
-console.log(`${andNow}`);
+// parse the relevant data into an object
+    //relevant data: 
+    /* 
+    job title
+    job id number
+    job posting url
+    job description
+    */
 
+// write this info as a JSON or some format, into a new file with unique name
